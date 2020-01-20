8be8fc30707f428a487646c88c58bc57673e4372
yatatsu <yatatsukitagawa@gmail.com>
2020-01-20 15:08:46 +0900

    Remove example

--- example/babel.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-module.exports = function(api) {
-    api.cache(true);
-    return {
-        presets: ["babel-preset-expo"]
-    };
-};
--- example/index.js
+++
@@ -1,9 +0,0 @@
-import { AppRegistry, Platform } from "react-native";
-import App from "./App";
-
-AppRegistry.registerComponent("example", () => App);
-
-if (Platform.OS === "web") {
-    const rootTag = document.getElementById("root") || document.getElementById("main");
-    AppRegistry.runApplication("example", { rootTag });
-}
