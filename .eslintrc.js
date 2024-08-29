750fac415b1f39108e2bc9b254e30070c599a76c
yatatsu <yatatsukitagawa@gmail.com>
2024-08-30 06:43:09 +0900

    Update dependencies for RN 0.74.5

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,6 @@
 module.exports = {
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:prettier/recommended",
-        "prettier/@typescript-eslint",
-        "plugin:react/recommended"
-    ],
-    plugins: ["@typescript-eslint"],
-    parser: "@typescript-eslint/parser",
-    env: {
-        browser: true,
-        node: true,
-        es6: true
-    },
-    settings: {
-        react: {
-            version: "detect"
-        }
-    },
-    parserOptions: {
-        sourceType: "module",
-        ecmaFeatures: {
-            jsx: true
-        }
-    },
-    rules: {
-        "@typescript-eslint/no-use-before-define": [
-            "error",
-            {
-                variables: false
-            }
-        ]
-    },
+    extends: '@react-native',
     globals: {
-        __DEV__: true
-    }
+        __DEV__: true,
+    },
 };
