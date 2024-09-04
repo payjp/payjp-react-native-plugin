166ce9e592d2e87b7eeaa741651b04b17be4337d
yatatsu <yatatsukitagawa@gmail.com>
2024-09-05 02:48:00 +0900

    test: fix emulator for e2e in CI.

--- example/.detoxrc.js
+++ example/.detoxrc.js
@@ -48,7 +48,7 @@ module.exports = {
         emulator: {
             type: 'android.emulator',
             device: {
-                avdName: 'Pixel 7 API 34',
+                avdName: 'Pixel 7',
             },
         },
     },
