3d423306f791c52c7f734c412c6dd7fd016043bb
yatatsu <yatatsukitagawa@gmail.com>
2024-09-05 00:01:51 +0900

    example: example2 -> example

--- Makefile
+++ Makefile
@@ -15,27 +15,27 @@ check: dependencies
 
 .PHONY: dependencies-example
 dependencies-example: dependencies
-	cd example2 && yarn --frozen-lockfile
+	cd example && yarn --frozen-lockfile
 
 .PHONY: check-example
 check-example: dependencies-example
-	cd example2 && yarn lint
+	cd example && yarn lint
 
 .PHONY: build-example-android
 build-example-android: dependencies-example
-	cd example2 && yes | yarn prebuild
-	cd example2/android && ./gradlew assembleDebug
+	cd example && yes | yarn prebuild
+	cd example/android && ./gradlew assembleDebug
 
 .PHONY: e2e-android
 e2e-android: dependencies-example
-	cd example2 && detox build --configuration android.emu.release
-	cd example2 && detox test --configuration android.emu.release --cleanup
+	cd example && detox build --configuration android.emu.release
+	cd example && detox test --configuration android.emu.release --cleanup
 
 .PHONY: dependencies-example-ios
 dependencies-example-ios: dependencies-example
-	cd example2 && yes | yarn prebuild
+	cd example && yes | yarn prebuild
 
 .PHONY: e2e-ios
 e2e-ios: dependencies-example
-	cd example2 && detox build --configuration ios.sim.release
-	cd example2 && detox test --configuration ios.sim.release --cleanup
+	cd example && detox build --configuration ios.sim.release
+	cd example && detox test --configuration ios.sim.release --cleanup
--- example/scripts/reset-project.js
+++ example/scripts/reset-project.js
@@ -42,32 +42,32 @@ export default function RootLayout() {
 }
 `;
 
-fs.rename(oldDirPath, newDirPath, (error) => {
-  if (error) {
-    return console.error(`Error renaming directory: ${error}`);
-  }
-  console.log('/app moved to /app-example.');
-
-  fs.mkdir(newAppDirPath, { recursive: true }, (error) => {
+fs.rename(oldDirPath, newDirPath, error => {
     if (error) {
-      return console.error(`Error creating new app directory: ${error}`);
+        return console.error(`Error renaming directory: ${error}`);
     }
-    console.log('New /app directory created.');
+    console.log('/app moved to /app-example.');
 
-    const indexPath = path.join(newAppDirPath, 'index.tsx');
-    fs.writeFile(indexPath, indexContent, (error) => {
-      if (error) {
-        return console.error(`Error creating index.tsx: ${error}`);
-      }
-      console.log('app/index.tsx created.');
-
-      const layoutPath = path.join(newAppDirPath, '_layout.tsx');
-      fs.writeFile(layoutPath, layoutContent, (error) => {
+    fs.mkdir(newAppDirPath, { recursive: true }, error => {
         if (error) {
-          return console.error(`Error creating _layout.tsx: ${error}`);
+            return console.error(`Error creating new app directory: ${error}`);
         }
-        console.log('app/_layout.tsx created.');
-      });
+        console.log('New /app directory created.');
+
+        const indexPath = path.join(newAppDirPath, 'index.tsx');
+        fs.writeFile(indexPath, indexContent, error => {
+            if (error) {
+                return console.error(`Error creating index.tsx: ${error}`);
+            }
+            console.log('app/index.tsx created.');
+
+            const layoutPath = path.join(newAppDirPath, '_layout.tsx');
+            fs.writeFile(layoutPath, layoutContent, error => {
+                if (error) {
+                    return console.error(`Error creating _layout.tsx: ${error}`);
+                }
+                console.log('app/_layout.tsx created.');
+            });
+        });
     });
-  });
 });
