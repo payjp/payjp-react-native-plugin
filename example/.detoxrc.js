a7c5de3dafb608dbd71e5fdbe945a38b410b7199
yatatsu <yatatsukitagawa@gmail.com>
2024-09-05 10:35:50 +0900

    Fix e2e emulator

--- example/.detoxrc.js
+++ example/.detoxrc.js
@@ -48,7 +48,7 @@ module.exports = {
         emulator: {
             type: 'android.emulator',
             device: {
-                avdName: 'Pixel 7',
+                avdName: 'Pixel_7',
             },
         },
     },
